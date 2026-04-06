0621aad88533586b1d2773fa7861c87bbe75d656
DasariBhanushree <dasaribhanushree@gmail.com>
2026-04-06 16:31:57 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"Rolling Update 🚀 | Pod: {socket.gethostname()}"
+    return f"Handled by pod: {socket.gethostname()}"
+
+@app.route("/health")
+def health():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
